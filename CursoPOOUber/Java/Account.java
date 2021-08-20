62770c5f3106433cbb658c004c497ff560609b5b
Javier Gutierrez (Yav) <gutierrez.alcantara.javier@gmail.com>
2021-08-20 16:41:38 -0500

    Agregado de clases en java

---
+++ CursoPOOUber/Java/Account.java
@@ -0,0 +1,7 @@
+class Account {
+  Integer Id;
+  String name;
+  String document;
+  String email;
+  String password;
+}
---
+++ CursoPOOUber/Java/Route.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+class Route {
+  Integer id;
+  ArrayList<Double> star;
+  ArrayList<Double> end;
+}
+
+// double ejemplifica el el tipo de dato decimal.
