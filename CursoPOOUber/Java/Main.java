804a8e5dae19476d4ccb84d21dcc2b7707e16740
Javier Gutierrez (Yav) <gutierrez.alcantara.javier@gmail.com>
2021-08-20 15:52:05 -0500

    Add del archivo main

---
+++ CursoPOOUber/Java/Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hola mundo");
+  }
+}
+
+// la clase debe tener el mismo nombre del archivo, public static sera la funcion que debemos pasar a la clase, y dentro podemos colocar el comportamiento 
