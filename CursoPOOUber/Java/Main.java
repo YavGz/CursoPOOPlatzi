7b7308e7a3f99a871d241afc2541baf15edc41c3
Javier Gutierrez (Yav) <gutierrez.alcantara.javier@gmail.com>
2021-08-24 19:40:26 -0500

    Creando el objeto car en java y python

--- CursoPOOUber/Java/Main.java
+++ CursoPOOUber/Java/Main.java
@@ -1,6 +1,18 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hola mundo");
+
+    Car car = new Car();
+    car.license = "AQM123";
+    car.driver = "Pedrito Sola";
+    car.passenger = 4;
+    car.printDataCar();
+  
+    Car car2 = new Car();
+    car2.license = "QWE453";
+    car2.driver = "Lolita Ayala";
+    car2.passenger = 3;
+    car2.printDataCar();
   }
 }
 
--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,2 +1,13 @@
+from car import Car
+
 if __name__ == "_main_":
   print("Hola Mundo")
+  car = Car()
+  car.license = "AQM123"
+  car.driver = "Pedrito Sola"
+  print(vars(car))
+
+  car2 = Car ()
+  car2.iicense = "QWE456"
+  car2.driver = "Carmen Salinas"
+  print(vars(car2))
