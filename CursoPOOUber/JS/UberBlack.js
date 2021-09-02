803a59e8db709e0389a669eff2b4d98885fd8654
Javier Gutierrez (Yav) <gutierrez.alcantara.javier@gmail.com>
2021-09-01 19:01:45 -0500

    Herencia y metodo constructor super en Js

---
+++ CursoPOOUber/JS/UberBlack.js
@@ -0,0 +1,7 @@
+class UberBlack extends Car {
+  constructor (license, driver, typeCArAccepted, seatsMaterial) {
+    super(license, driver);
+    this.typeCArAccepted = typeCArAccepted;
+    this.seatsMaterial = seatsMaterial;
+  }
+}
--- CursoPOOUber/JS/index.js
+++ CursoPOOUber/JS/index.js
@@ -1,3 +1,7 @@
-var car = new Car("TOP123", new Account("Pedrito Sola", "KEA12"));
+let car = new Car("TOP123", new Account("Pedrito Sola", "KEA12"));
 car.passenger = 4;
 car.printDataCar();
+
+let uberX = new UberX("ASD123", new Account("Lolita Ayala","ASFF453"), "Chevrolet", "A");
+uberX.passenger = 6;
+uberX.printDataCar();
